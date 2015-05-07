03428693ce917262e7737f9a0d252f2487e779d8
ericsherman <esherm@mail.med.upenn.edu>
2015-05-07 09:31:23 -0400

    fixed bug where if a sample failed trimming, the (potentially) wrong sample would be excluded from site calling

--- postTrimSeqs_PMACS.R
+++ postTrimSeqs_PMACS.R
@@ -1,4 +1,6 @@
-metadata = read.csv("sampleInfo.csv") #just need aliases, so sampleInfo.csv is ok
+#sample index in callIntSites_PMACS.R is based on parameters.RData, so we
+#should pass appropriate index values by using parameters.RData here
+metadata = get(load("parameters.RData"))
 
 bushmanJobID = get(load("bushmanJobID.RData"))
 blatStartPort = get(load("bushmanBlatStartPort.RData"))
@@ -11,8 +13,8 @@ numFastaFiles = length(system("ls */*.fa", intern=T))
 system(paste0('bsub -n1 -q normal -w "done(BushmanPostTrimProcessing_', bushmanJobID, ')" -J "BushmanAlignSeqs_', bushmanJobID, '[', blatStartPort, '-', blatStartPort+numFastaFiles-1, ']" -o logs/alignOutput%I.txt Rscript ~/EAS/PMACS_scripts/alignSeqs_PMACS.R'))
 system("sleep 10")
 #call int sites (have to find out which ones worked)
-successfulTrims = unlist(sapply(metadata$alias, function(x){
-  get(load(paste0(x, "/trimStatus.RData"))) == paste0(getwd(), "/", x)
+successfulTrims = unlist(sapply(metadata, function(x){
+  get(load(paste0(x[["alias"]], "/trimStatus.RData"))) == paste0(getwd(), "/", x[["alias"]])
 }))
 
 system(paste0('bsub -n1 -q max_mem30 -w "done(BushmanAlignSeqs_', bushmanJobID, ')" -J "BushmanCallIntSites_', bushmanJobID, '[', paste(which(successfulTrims), collapse=","), ']" -o logs/callSitesOutput%I.txt Rscript ~/EAS/PMACS_scripts/callIntSites_PMACS.R'))
