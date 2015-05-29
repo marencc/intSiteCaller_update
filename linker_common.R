0db81141085be38ea862d0e8db7fd232db73fd20
yinghua <yinghua@upenn.edu>
2015-05-29 11:33:20 -0400

    Seems to be working, past tests, need to test more

--- linker_common.R
+++ linker_common.R
@@ -2,5 +2,14 @@
 #' @param linkerSequence, vector of linker sequences
 #' @return linkerCommon, vector of the sequence after N
 linker_common <- function(linkerSequence) {
-    return(linkerSequence)
+    stopifnot(require("Biostrings"))
+    splited <- strsplit(linkerSequence, 'N+')
+    stopifnot(sapply(splited, length) == 2)
+    afterN <- sapply(splited, "[[", 2)
+    stopifnot(length(afterN)==length(linkerSequence))
+    
+    afterN_RCed <- unlist(lapply(afterN, function(x) as.character(reverseComplement(DNAString(x)))))
+    stopifnot(length(afterN_RCed)==length(linkerSequence))
+    
+    return(afterN_RCed)
 }
