d9664f78d5354c8eedd1e7eab53399e9fd42259d
Anatoly Dryga <anatolydryga@gmail.com>
2015-06-03 13:36:07 -0400

    sanity checks for column names, gender, sample names(aliases)

--- read_sample_files.R
+++ read_sample_files.R
@@ -12,9 +12,34 @@ read_sample_processing_files <- function(sample_info_path, processing_info_path)
     sampleInfo <- read.delim(sample_info_path, stringsAsFactors=F)
     sampleInfo$linkerCommon <- linker_common(sampleInfo$linkerSequence)
     processingParams <- read.delim(processing_info_path, stringsAsFactors=F)
-    cbind(sampleInfo, processingParams)
+    complete_meta <- cbind(sampleInfo, processingParams)
+    .check_column_names(complete_meta)
+    .check_sex_is_correct(complete_meta$gender)
+    .check_sampleName_is_correct(complete_meta$alias)
+    complete_meta
 }
 
 .check_input <- function(sample_info_path, processing_info_path) {
     stopifnot(file.exists(sample_info_path) & file.exists(processing_info_path))
+}
+
+.columns_sample_proc <- c( "qualityThreshold", "badQualityBases", "qualitySlidingWindow",
+    "primer", "ltrBit", "largeLTRFrag", "linkerSequence", "linkerCommon",
+    "mingDNA", "alias", "vectorSeq", "minPctIdent",
+    "maxAlignStart", "maxFragLength", "gender"
+)
+
+.check_column_names <- function(samples) {
+    stopifnot(all(.columns_sample_proc %in% names(samples)))
+}
+
+.valid_sex <- c('m', 'f')
+
+.check_sex_is_correct <- function(gender) {
+    stopifnot(all(gender %in% .valid_sex))
+}
+
+.check_sampleName_is_correct <- function(samples) {
+    stopifnot( ! (is.null(samples)))
+    stopifnot(length(samples) == length(unique(samples)))
 }
