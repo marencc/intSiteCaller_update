538c7562d3c057f50b71259122fdc1db948dbbd6
yhwu <ywu9312@gmail.com>
2015-08-02 18:06:30 -0400

    add Rmd

--- html_stats.R
+++ html_stats.R
@@ -1,3 +1,6 @@
+libs <- (c("plyr", "ggplot2", "reshape2", "knitr"))
+sapply(libs, require, character.only=TRUE)
+
 options(stringsAsFactors = FALSE)
 
 cur_dir <- getwd()
@@ -26,35 +29,50 @@ stats$gtsp <- NULL
 
 stats.mdf <-  melt(stats, id.vars="sample")
 stats.mdf$gtsp <- sub("-\\d+$", "", stats.mdf$sample)
-    
+stats.mdf$replicate <- sub("GTSP\\d+-", "", stats.mdf$sample)
+
 stats.mdf.samplelist <- split(stats.mdf, stats.mdf$gtsp)
 
-ggplot(subset(stats.mdf, grepl("GTSP0659-1", sample)), aes(value, variable)) + geom_point() + facet_wrap( ~ sample, ncol=4)
-
-ggplot(subset(stats.mdf, grepl("^GTSP0659-", sample)), aes(variable, value)) + geom_point() +
-    facet_wrap( ~ sample, ncol=1) +
-        theme(axis.text.x = element_text(angle = 45, hjust = 1))
-
-
-theme_default <- theme(text = element_text(size=14),
-                       axis.title.x=element_blank(),
-                       axis.title.y=element_blank(),
-                       axis.text.x = element_text(size=14, face="bold"),
-                       axis.title.x = element_text(size=14, face="bold"),
-                       axis.text.y = element_text(size=14, face="bold"),
-                       axis.title.y = element_text(size=14, face="bold"),
-                       ##legend.position=c(0.9,0.85),
-                       legend.key.size=1,
-                       legend.text=element_text(size=8),
-                       legend.position="top",
-                       legend.box = "horizontal",
-                       legend.title = element_blank())
+theme_default <- theme_bw() + 
+    theme(text = element_text(size=14),
+          axis.title.x=element_blank(),
+          axis.title.y=element_blank(),
+          axis.text.x = element_text(size=14, face="bold", angle = 45, hjust = 1),
+          axis.title.x = element_text(size=14, face="bold"),
+          axis.text.y = element_text(size=14, face="bold"),
+          axis.title.y = element_text(size=14, face="bold"),
+          ##legend.position=c(0.9,0.85),
+          ##legend.key.size=1,
+          legend.text=element_text(size=8),
+          legend.position="top",
+          legend.box = "horizontal",
+          legend.title = element_text("Replicates"))
 
 
-ggplot(plyr::rbind.fill(stats.mdf.samplelist[1:4]), aes(variable, value, fill=sample)) +
+ggplot(plyr::rbind.fill(stats.mdf.samplelist[1:4]), aes(variable, value, fill=replicate)) +
     geom_bar(position=position_dodge(width = 0.8), stat="identity") + 
-        theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
             theme_default +
                 facet_wrap( ~ gtsp, ncol=1, scales = "free_y")
 
 
+ggplot(plyr::rbind.fill(stats.mdf.samplelist[ plotList[[i]] ]), 
+       aes(variable, value, fill=replicate)) +
+    geom_bar(position=position_dodge(width = 0.8), stat="identity") + 
+    theme_default +
+    facet_wrap( ~ gtsp, ncol=1, scales = "free_y")
+
+
+plotsPerPage <- 4
+plotList <- split(1:length(stats.mdf.samplelist), 
+                  (1:length(stats.mdf.samplelist)-1)%/%plotsPerPage)
+
+for(i in seq(plotList)) {
+    ##cat(plotList[[i]],"\n")
+    
+    ggplot(plyr::rbind.fill(stats.mdf.samplelist[ plotList[[i]] ]), 
+           aes(variable, value, fill=replicate)) +
+        geom_bar(position=position_dodge(width = 0.8), stat="identity") + 
+        theme_default +
+        facet_wrap( ~ gtsp, ncol=1, scales = "free_y")
+    
+}
