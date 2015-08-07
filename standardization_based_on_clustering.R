5500fdc07d7903c8e2ecb8a3e7732c26d4576ee2
Chris Nobles <chris.l.nobles@gmail.com>
2015-08-07 15:28:37 -0400

    Reduce by 0L as sites are already standardized.

--- standardization_based_on_clustering.R
+++ standardization_based_on_clustering.R
@@ -30,24 +30,24 @@ standardizeSites <- function(unstandardizedSites){
   sort(standardized)
 }  
 
-  dereplicateSites <- function(sites){
-    #Reduce sites which have the same starts, but loose range info
-    #(no need to add a gapwidth as sites are standardized)
-    sites.reduced <- flank(sites, -1, start=TRUE)
-    sites.reduced <- unlist(reduce(sites.reduced, with.revmap=TRUE))
-    sites.reduced$counts <- sapply(sites.reduced$revmap, length)
-    
-    #Order original sites by revmap  
-    dereplicatedSites <- sites[unlist(sites.reduced$revmap)]
-    
-    #Skip this step and provide similar output if length(sites) = 0
-    if(length(sites) > 0){
-      dereplicatedSites <- split(dereplicatedSites, Rle(values = seq(length(sites.reduced)), lengths = sites.reduced$counts))
-    }  
-    
-    #Dereplicate reads with same standardized starts and provide the longeset width
-    dereplicatedSites <- unlist(reduce(dereplicatedSites))
-    mcols(dereplicatedSites) <- mcols(sites.reduced)
+dereplicateSites <- function(sites){
+  #Reduce sites which have the same starts, but loose range info
+  #(no need to add a gapwidth as sites are standardized)
+  sites.reduced <- flank(sites, -1, start=TRUE)
+  sites.reduced <- unlist(reduce(sites.reduced, min.gapwidth = 0L, with.revmap=TRUE))
+  sites.reduced$counts <- sapply(sites.reduced$revmap, length)
+  
+  #Order original sites by revmap  
+  dereplicatedSites <- sites[unlist(sites.reduced$revmap)]
+  
+  #Skip this step and provide similar output if length(sites) = 0
+  if(length(sites) > 0){
+    dereplicatedSites <- split(dereplicatedSites, Rle(values = seq(length(sites.reduced)), lengths = sites.reduced$counts))
+  }  
+  
+  #Dereplicate reads with same standardized starts and provide the longeset width
+  dereplicatedSites <- unlist(reduce(dereplicatedSites))
+  mcols(dereplicatedSites) <- mcols(sites.reduced)
 
-    dereplicatedSites
-  }
+  dereplicatedSites
+}
