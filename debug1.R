8305ae652536ce8ca6806aec79ad2567396f9bec
yhwu <ywu9312@gmail.com>
2015-11-22 17:29:02 -0500

    trim over read finallized

--- debug1.R
+++ debug1.R
@@ -1,12 +1,12 @@
-PairwiseAlignmentsSingleSubject2DF <- function(PA) {
+PairwiseAlignmentsSingleSubject2DF <- function(PA, shift=0) {
     stopifnot("PairwiseAlignmentsSingleSubject"  %in% class(PA))
     
     return(data.frame(
         width=width(pattern(PA)),
         score=score(PA),
         mismatch=width(pattern(PA))-score(PA),
-        start=start(pattern(PA)),
-        end=end(pattern(PA))
+        start=start(pattern(PA))+shift,
+        end=end(pattern(PA))+shift
         ))
 }
 
@@ -14,6 +14,9 @@ PairwiseAlignmentsSingleSubject2DF <- function(PA) {
 trim_overreading <- function(reads, marker, maxMisMatch=3) {
     reads=reads.p
     marker=linker_common
+    maxMisMatch=3
+    reads=reads.l
+    marker=substr(largeLTRFrag,1,20)
     maxMisMatch=3
     
     stopifnot(class(reads) %in% "DNAStringSet")
@@ -35,36 +38,45 @@ trim_overreading <- function(reads, marker, maxMisMatch=3) {
     odf <- PairwiseAlignmentsSingleSubject2DF(tmp)
     
     odf$isgood <- FALSE
-    ## aligned to middle or right
+    ## overlap in the middle or at right
     odf$isgood <- with(odf, ifelse(mismatch<=maxMisMatch &
                                    start>1,
                                    TRUE, isgood))
     
-    ## aligned to left
+    ## overlap at left
     odf$isgood <- with(odf, ifelse(mismatch<=maxMisMatch &
                                    start==1 &
                                    width>=nchar(marker)-1,
                                    TRUE, isgood))
     
+    ## note with ovelrap alignmment, it only align with a minimum of 1/2 of the shorter one
+    odf$cut <- with(odf, ifelse(isgood, odf$start-1, nchar(reads)))
+    if( any(odf$cut < nchar(reads)) ) {
+        odf$cut <- nchar(reads)-nchar(marker)/2
+        odf$cut <- with(odf, ifelse(isgood, odf$start-1, cut))
+    }
     
-    mwidth <- as.integer(stringr::str_match(names(reads), "(\\d+):(\\d+)$")[,2])
+    reads <- subseq(reads, 1, odf$cut)
     
-    ovldf <- data.frame(size=nchar(linker_common),
-                        width=width(pattern(tmp)),
-                        score=score(tmp),
-                        mismatch=width(pattern(tmp))-score(tmp),
-                        start=start(pattern(tmp)),
-                        end=end(pattern(tmp)),
-                        rlen=nchar(reads),
-                        mwidth=mwidth,
-                        cut=ifelse( mwidth<nchar(reads), mwidth+1, nchar(reads) ))
-    
-    ovldf$good <- ( abs(ovldf$cut-ovldf$start)<=2 )
-    
-    library(rpart)
-    fit <- rpart(good ~ size + width + score + mismatch + start + end + rlen,
-                 data=ovldf,
-                 method="class")
+
+    ## debugging and machine learning code
+    ## for linker_common, 16 bases, maxMisMatch=3 is the best
+    ## mwidth <- as.integer(stringr::str_match(names(reads), "(\\d+):(\\d+)$")[,2])
+    ## truecut <- ifelse( mwidth<nchar(reads), mwidth+1, nchar(reads) )
+    ## ovldf <- data.frame(size=nchar(linker_common),
+    ##                    width=width(pattern(tmp)),
+    ##                    score=score(tmp),
+    ##                    mismatch=width(pattern(tmp))-score(tmp),
+    ##                    start=start(pattern(tmp)),
+    ##                    end=end(pattern(tmp)),
+    ##                    rlen=nchar(reads),
+    ##                    mwidth=mwidth,
+    ##                    cut=ifelse( mwidth<nchar(reads), mwidth+1, nchar(reads) ))
+    ## ovldf$good <- ( abs(ovldf$cut-ovldf$start)<=2 )
+    ## library(rpart)
+    ## fit <- rpart(good ~ size + width + score + mismatch + start + end + rlen,
+    ##             data=ovldf,
+    ##             method="class")
 }
 
 
