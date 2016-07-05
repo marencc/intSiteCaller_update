4dec43494f2d6ed864e0a89b844345fd5c0467db
John Everett <everett.jk@gmail.com>
2016-07-05 11:30:29 -0400

    Update make_primaryAnalysisDirectory.R

--- make_primaryAnalysisDirectory.R
+++ make_primaryAnalysisDirectory.R
@@ -1,3 +1,20 @@
+#    This source code file is a component of the larger INSPIIRED genomic analysis software package.
+#    Copyright (C) 2016 Frederic Bushman
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
 #' 1. decide the runid from the current directory, e.g,
 #'    within run20150505, the runid is 150505_M03249_0067_000000000-AFCKK
 #' 2. pull fastq.gz file from microb120:/media/THING1 for runid
